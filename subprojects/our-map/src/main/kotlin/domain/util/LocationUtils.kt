494c1aa7f347a1add4003fe3c7d851f6b6fa8da3
Swan <jsh56son@gmail.com>
2021-07-05 21:54:25 +0900

    장소 검색 시 가까운 장소가 먼저 노출되는 테스트 작성

--- subprojects/our-map/src/main/kotlin/domain/util/LocationUtils.kt
+++ subprojects/our-map/src/main/kotlin/domain/util/LocationUtils.kt
@@ -1,7 +1,12 @@
 package domain.util
 
+import org.geotools.referencing.GeodeticCalculator
+
 object LocationUtils {
     fun calculateDistance(l1: Location, l2: Location): Length {
-        return Length(0.0) // TODO
+        val calculator = GeodeticCalculator()
+        calculator.setStartingGeographicPoint(l1.lng, l1.lat)
+        calculator.setDestinationGeographicPoint(l2.lng, l2.lat)
+        return Length(calculator.orthodromicDistance)
     }
 }
--- subprojects/our-map/src/test/kotlin/domain/place/SearchPlaceServiceTest.kt
+++ subprojects/our-map/src/test/kotlin/domain/place/SearchPlaceServiceTest.kt
@@ -24,7 +24,7 @@ class SearchPlaceServiceTest : PlaceDomainTestBase() {
     }
 
     @Test
-    fun `정상적인 경우`() {
+    fun `검색이 잘 되어야 한다`() {
         val result1 = searchPlaceService.searchPlaces("수환", Location(0.0, 0.0))
         Assert.assertEquals(0, result1.size)
 
@@ -58,5 +58,62 @@ class SearchPlaceServiceTest : PlaceDomainTestBase() {
         Assert.assertEquals(0, result3.size)
         val result4 = searchPlaceService.searchPlaces("타", Location(0.0, 0.0)) // 한글자로는 검색이 안 되어야 한다.
         Assert.assertEquals(0, result4.size)
+    }
+
+    @Test
+    fun `가까운 장소가 먼저 노출되어야 한다`() {
+        val result1 = searchPlaceService.searchPlaces("수환", Location(0.0, 0.0))
+        Assert.assertEquals(0, result1.size)
+
+        val nearBuilding = buildingRepository.add(Building(
+            id = EntityIdRandomGenerator.generate(),
+            name = "아크로서울포레스트 D타워1",
+            lng = 0.05,
+            lat = 0.05,
+            address = BuildingAddress(
+                siDo = "서울특별시",
+                siGunGu = "성동구",
+                eupMyeonDong = "성수동",
+                li = "",
+                roadName = "왕십리로",
+                mainBuildingNumber = "83",
+                subBuildingNumber = "21"
+            )
+        ))
+        val nearPlace = placeRepository.add(Place(
+            id = EntityIdRandomGenerator.generate(),
+            name = "D타워1 장소",
+            lng = 0.05,
+            lat = 0.05,
+            building = nearBuilding
+        ))
+
+        val farBuilding = buildingRepository.add(Building(
+            id = EntityIdRandomGenerator.generate(),
+            name = "아크로서울포레스트 D타워2",
+            lng = 0.1,
+            lat = 0.1,
+            address = BuildingAddress(
+                siDo = "서울특별시",
+                siGunGu = "성동구",
+                eupMyeonDong = "성수동",
+                li = "",
+                roadName = "왕십리로",
+                mainBuildingNumber = "83",
+                subBuildingNumber = "21"
+            )
+        ))
+        val farPlace = placeRepository.add(Place(
+            id = EntityIdRandomGenerator.generate(),
+            name = "D타워2 장소",
+            lng = 0.1,
+            lat = 0.1,
+            building = nearBuilding
+        ))
+
+        val result = searchPlaceService.searchPlaces("D타워", Location(0.0, 0.0))
+        Assert.assertEquals(2, result.size)
+        Assert.assertEquals(nearPlace.id, result[0].id)
+        Assert.assertEquals(farPlace.id, result[1].id)
     }
 }
