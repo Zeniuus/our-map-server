9be73bd24f1921fb691bafbe5fb510154ff378c3
Swan <jsh56son@gmail.com>
2021-07-22 22:00:48 +0900

    test todo 적어놓기

---
+++ subprojects/our-map/src/test/kotlin/domain/village/VillageServiceTest.kt
@@ -0,0 +1,8 @@
+package domain.village
+
+class VillageServiceTest {
+    // TODO: upsert test
+    // TODO: list ranking test
+    // TODO: getMainViewData route test
+    // TODO: PlaceAccessibilityEventPublisher.accessibilityRegistered() test
+}
