141b00b7974bd0cf3b23563cbc0959a1b1d0ef21
Swan <jsh56son@gmail.com>
2021-07-23 22:16:57 +0900

    모든 Village 초기화하는 메소드 추가

--- subprojects/our-map/src/main/kotlin/application/village/VillageApplicationService.kt
+++ subprojects/our-map/src/main/kotlin/application/village/VillageApplicationService.kt
@@ -2,14 +2,25 @@ package application.village
 
 import application.TransactionManager
 import domain.village.entity.Village
+import domain.village.repository.EupMyeonDongRepository
 import domain.village.repository.VillageRepository
+import domain.village.service.VillageService
 
 class VillageApplicationService(
     private val transactionManager: TransactionManager,
     private val villageRepository: VillageRepository,
+    private val eupMyeonDongRepository: EupMyeonDongRepository,
+    private val villageService: VillageService,
 ) {
     fun listForMainView(): List<Village> = transactionManager.doInTransaction {
         villageRepository.listAll()
             .sortedByDescending { it.registerProgress }
+    }
+
+    fun insertAll() = transactionManager.doInTransaction {
+        val eupMyeonDongs = eupMyeonDongRepository.listAll()
+        eupMyeonDongs.forEach { eupMyeonDong ->
+            villageService.upsertStatistics(eupMyeonDong)
+        }
     }
 }
