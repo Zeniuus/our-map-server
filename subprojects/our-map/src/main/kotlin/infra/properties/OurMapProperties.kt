770f9aa0b5dd28994c9da2a97de77490426a3c34
Swan <jsh56son@gmail.com>
2021-08-08 12:07:33 +0900

    실섭 환경의 properties 파일을 도커 실행 시 지정할 수 있도록 변경 (refs: #11)

--- subprojects/our-map/src/main/kotlin/infra/properties/OurMapProperties.kt
+++ subprojects/our-map/src/main/kotlin/infra/properties/OurMapProperties.kt
@@ -1,15 +1,39 @@
 package infra.properties
 
+import java.io.File
 import java.util.Properties
 
 object OurMapProperties {
-    private val properties = javaClass.classLoader.getResourceAsStream("application.properties").use {
-        it?.let {
-            Properties().apply { load(it) }
-        } ?: Properties()
+    private val defaultProperties = loadProperties("application.properties")
+    private var overridingProperties: Properties? = null
+
+    init {
+        System.getenv("OUR_MAP_OVERRIDING_PROPERTIES_FILENAME")?.let {
+            overridingProperties = loadProperties(it)
+        }
+    }
+
+    private fun loadProperties(propertiesFilename: String): Properties {
+        val resource = javaClass.classLoader.getResourceAsStream(propertiesFilename)
+        if (resource != null) {
+            return resource.use {
+                it.let {
+                    Properties().apply { load(it) }
+                }
+            }
+        }
+        val file = File(propertiesFilename)
+        if (file.exists()) {
+            return file.inputStream().use {
+                it.let {
+                    Properties().apply { load(it) }
+                }
+            }
+        }
+        return Properties()
     }
 
     operator fun get(key: String): String? {
-        return properties.getProperty(key)
+        return overridingProperties?.getProperty(key) ?: defaultProperties.getProperty(key)
     }
 }
