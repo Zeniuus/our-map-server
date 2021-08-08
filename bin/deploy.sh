bcf61b6414b2e690070aec65730cead8678723a0
Swan <jsh56son@gmail.com>
2021-08-08 13:05:53 +0900

    main 브랜치에 대해 CD 붙이기 (refs: #12)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,7 @@
 
 aws ecr get-login-password --region us-west-2 | sudo docker login --username AWS --password-stdin 563057296362.dkr.ecr.us-west-2.amazonaws.com
 sudo docker pull 563057296362.dkr.ecr.us-west-2.amazonaws.com/our-map-server:latest
+sudo docker ps -a -q | xargs sudo docker rm -f # 기존에 떠 있던 서버를 종료한다.
 sudo nohup docker run -p 80:8080 \
   -v $(pwd)/deploy/prod:/app/conf \
   -e OUR_MAP_OVERRIDING_PROPERTIES_FILENAME=/app/conf/application-prod.properties \
