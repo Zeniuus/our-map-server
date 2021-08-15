1a3670d4f73f0d735bcdb3222c2568b5359d5fd5
Swan <jsh56son@gmail.com>
2021-08-15 15:13:37 +0900

    빌드 오류 수정

--- bin/docker-push.sh
+++ bin/docker-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 563057296362.dkr.ecr.us-west-2.amazonaws.com
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 563057296362.dkr.ecr.ap-northeast-2.amazonaws.com
 ./gradlew installDist # FIXME: 분명 dockerPushOurMap이 installDist에 의존하는데... 이 줄이 없으면 build/install/our-map-server 아래 내용이 아무것도 복사가 안 된다.
 ./gradlew dockerPushOurMap
