69dca338fe8361cb15e941e004e66bdcea34aa7f
Swan <jsh56son@gmail.com>
2021-08-27 11:05:04 +0900

    access token의 userId에 해당하는 유저가 없는 경우 401 에러 내기

--- subprojects/our-map/src/main/kotlin/application/user/UserApplicationModule.kt
+++ subprojects/our-map/src/main/kotlin/application/user/UserApplicationModule.kt
@@ -4,5 +4,5 @@ import org.koin.dsl.module
 
 val userApplicationModule = module {
     single { UserApplicationService(get(), get(), get(), get()) }
-    single { UserAuthApplicationService(get(), get()) }
+    single { UserAuthApplicationService(get(), get(), get()) }
 }
--- subprojects/our-map/src/main/kotlin/application/user/UserAuthApplicationService.kt
+++ subprojects/our-map/src/main/kotlin/application/user/UserAuthApplicationService.kt
@@ -1,14 +1,24 @@
 package application.user
 
 import application.TransactionManager
+import domain.user.exception.UserAuthenticationException
+import domain.user.repository.UserRepository
 import domain.user.service.UserAuthService
+import java.sql.SQLException
 
 class UserAuthApplicationService(
     private val transactionManager: TransactionManager,
+    private val userRepository: UserRepository,
     private val userAuthService: UserAuthService,
 ) {
     // User ID를 반환한다.
     fun verify(accessToken: String): String = transactionManager.doInTransaction {
-        userAuthService.verifyAccessToken(accessToken).userId
+        val userId = userAuthService.verifyAccessToken(accessToken).userId
+        try {
+            userRepository.findById(userId)
+        } catch (e: SQLException) {
+            throw UserAuthenticationException()
+        }
+        userId
     }
 }
