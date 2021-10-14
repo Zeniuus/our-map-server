918c335805145c15748e4db8eee52bfdbe7e8eec
Swan <jsh56son@gmail.com>
2021-10-15 01:16:20 +0900

    로그인 화면 / 퀘스트 상세 화면에서는 내비게이션 바 없애기

--- frontend-admin/admin/src/App.tsx
+++ frontend-admin/admin/src/App.tsx
@@ -1,18 +1,67 @@
-import React from "react";
-import { BrowserRouter } from 'react-router-dom';
+import React, { useState } from 'react';
 import { Classes } from "@blueprintjs/core";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import apiClient from './apiClient';
 import AppMenu from "./AppMenu";
-import AppBody from "./AppBody";
+import Home from './page/Home';
+import RunSql from './page/RunSql/RunSql';
+import ClubQuests from './page/ClubQuests/ClubQuests';
+import ClubQuest from './page/ClubQuest/ClubQuest';
+import AuthRoute from './AuthRoute';
+import Login from './page/Login/Login';
 
 import "./App.scss";
 
-const App = () => (
-  <div className={Classes.RUNNING_TEXT}>
-    <BrowserRouter>
-      <AppMenu></AppMenu>
-      <AppBody></AppBody>
-    </BrowserRouter>
-  </div>
-);
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  function login(userId: string, password: string): Promise<any> {
+    if (userId === 'admin' && password === '2021stair!') { // TODO: ㅋㅋㅋㅋ 완전 땜빵
+      return Promise.resolve()
+        .then(() => {
+          setIsAuthenticated(true);
+        });
+    } else {
+      return Promise.reject();
+    }
+    // return apiClient.post('/login', {
+    //   userId,
+    //   password,
+    // }).then(() => {
+    //   setIsAuthenticated(true);
+    // }, () => {
+    //   alert('로그인에 실패했습니다.');
+    // });
+  }
+
+  const AppBody = (Child: any, otherProps: any) => {
+    return (props: any) => (
+      <div className="app-body">
+        <Child {...props} {...otherProps} />
+      </div>
+    );
+  }
+
+  return (
+    <div className={Classes.RUNNING_TEXT}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={AppBody(Login, { login, isAuthenticated })} />
+          <Route exact path="/clubQuests/:id" component={AppBody(ClubQuest, {})} />
+          <Route>
+            <AppMenu />
+            <div className="app-body">
+              <Switch>
+                <AuthRoute isAuthenticated={isAuthenticated} path="/runSql" component={RunSql} />
+                <AuthRoute isAuthenticated={isAuthenticated} path="/clubQuests" component={ClubQuests} />
+                <AuthRoute isAuthenticated={isAuthenticated} path="/" component={Home} />
+              </Switch>
+            </div>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
 
 export default App;
