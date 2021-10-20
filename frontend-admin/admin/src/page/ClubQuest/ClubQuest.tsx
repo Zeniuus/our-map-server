14ef5fd29760dcf94250c95d0f1fc771f31aaf25
Swan <jsh56son@gmail.com>
2021-10-21 01:00:25 +0900

    어드민 퀘스트 지도에 현재 위치 보여주기

--- frontend-admin/admin/src/page/ClubQuest/ClubQuest.tsx
+++ frontend-admin/admin/src/page/ClubQuest/ClubQuest.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Button, ButtonGroup } from '@blueprintjs/core';
 import haversine from 'haversine';
 import apiClient from '../../apiClient';
 import { Location } from '../../type';
@@ -21,6 +22,8 @@ interface ClubQuestProps {
 
 function ClubQuest(props: ClubQuestProps) {
   const [clubQuest, setClubQuest] = useState<ClubQuest | null>(null);
+  const [currentLocation, setCurrentLocation] = useState<Location | null>(null);
+  const [map, setMap] = useState<any>(null);
 
   useEffect(() => {
     getClubQuest(props.match.params.id);
@@ -44,6 +47,7 @@ function ClubQuest(props: ClubQuestProps) {
         level: determineLevel(clubQuest.content.targets),
       };
       const map = new window.kakao.maps.Map(container, options);
+      setMap(map);
 
       clubQuest.content.targets.forEach((target) => {
         const marker = new window.kakao.maps.Marker({
@@ -60,6 +64,34 @@ function ClubQuest(props: ClubQuestProps) {
           tooltip.open(map, marker);
         });
       });
+
+      if (navigator.geolocation != null) {
+        let currentLocationMarker: any = null;
+        const updateCurrentLocationMarker = () => {
+          navigator.geolocation.getCurrentPosition((position) => {
+            setCurrentLocation({ lat: position.coords.latitude, lng: position.coords.longitude });
+
+            if (currentLocationMarker != null) {
+              currentLocationMarker.setMap(null);
+            }
+
+            const markerImage = new window.kakao.maps.MarkerImage(
+              '/currentLocation.jpg',
+              new window.kakao.maps.Size(24, 24),
+              { offset: new window.kakao.maps.Point(12, 12) },
+            );
+            currentLocationMarker = new window.kakao.maps.Marker({
+              position: new window.kakao.maps.LatLng(position.coords.latitude, position.coords.longitude),
+              image: markerImage,
+              clickable: true, // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다.
+            });
+            currentLocationMarker.setMap(map);
+          });
+        }
+
+        updateCurrentLocationMarker();
+        setInterval(updateCurrentLocationMarker, 5000);
+      }
     }
   }
 
@@ -133,10 +165,27 @@ function ClubQuest(props: ClubQuestProps) {
     return max;
   }
 
+  const showQuestsOnMap = () => {
+    if (clubQuest != null) {
+      const center = determineCenter(clubQuest.content.targets);
+      map.setLevel(determineLevel(clubQuest.content.targets));
+      map.panTo(new window.kakao.maps.LatLng(center.lat, center.lng));
+    }
+  };
+  const showCurrentLocationOnMap = () => {
+    if (clubQuest != null && currentLocation != null) {
+      map.panTo(new window.kakao.maps.LatLng(currentLocation?.lat, currentLocation?.lng));
+    }
+  }
+
   return (
     <div>
       <h1>{clubQuest?.title}</h1>
       <div id="map" style={{ minWidth: '320px', minHeight: '500px' }} />
+      <ButtonGroup className="map-manipulate-button-container">
+        {clubQuest != null ? <Button text="퀘스트 전체 표시하기" onClick={showQuestsOnMap}></Button> : null}
+        {currentLocation != null ? <Button text="현재 위치 표시하기" onClick={showCurrentLocationOnMap}></Button> : null}
+      </ButtonGroup>
     </div>
   );
 }
